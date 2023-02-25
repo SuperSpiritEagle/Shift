436d973f4180a45c289b9825c7d53bffee171f74
SuperSpiritEagle <118557578+SuperSpiritEagle@users.noreply.github.com>
2023-02-25 13:12:23 +0500

    init

---
+++ Shifting/Shifting/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Shifting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int Elements = 5;
+            int[] numbers = new int[Elements] { 1, 2, 3, 4, 5 };
+            int shift;
+            int temp;
+
+            for (int i = 0; i < Elements; i++)
+            {
+                Console.Write(numbers[i]);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("На сколько циклически сдвинуть массив");
+
+            shift = Convert.ToInt32(Console.ReadLine());
+            while (shift > 0)
+            {
+                temp = numbers[Elements-Elements];
+
+                for (int i = 1; i < Elements; i++)
+                {
+                    numbers[i - 1] = numbers[i];
+                }
+
+                numbers[Elements-1] = temp;
+                shift--;
+            }
+
+            for (int i = 0; i < Elements; i++)
+            {
+                Console.Write(numbers[i]);
+            }
+        }
+    }
+}
